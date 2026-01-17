43d2847ae27ef3580a6c5675de30334365754498
Craig Johnson <craigjohnson@forwardnetworks.com>
2026-01-17 15:38:10 -0600

    fix: make deployment type check safe

---
+++ internal/skyforgedb/migrations/20260117000300_fix_deployment_type_check.up.sql
@@ -0,0 +1,18 @@
+UPDATE sf_deployments
+SET type='terraform'
+WHERE type='tofu';
+
+ALTER TABLE sf_deployments
+  DROP CONSTRAINT IF EXISTS sf_deployments_type_check;
+
+ALTER TABLE sf_deployments
+  ADD CONSTRAINT sf_deployments_type_check
+  CHECK ((type = ANY (ARRAY[
+    'terraform'::text,
+    'netlab'::text,
+    'netlab-c9s'::text,
+    'labpp'::text,
+    'containerlab'::text,
+    'clabernetes'::text
+  ])));
+
